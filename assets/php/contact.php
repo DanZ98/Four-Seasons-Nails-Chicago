216638fc17cf7d0c423bef3924ce4875ae05d215
Dan Zins <danzins15@gmail.com>
2020-06-01 23:30:24 -0500

    Initialize images

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -1,17 +1,22 @@
-<?php
+<?php 
 
-$name = $_REQUEST['name'];
-$email = $_REQUEST['email'];
-$tel = $_REQUEST['tel'];
-$email_body = 'Name: $name. \n'.
-              'Email: $email. \n'.
-              'Telephone: $tel. \n';
-$message = $_REQUEST['message'];
-$headers = 'From: danzins15@gmail.com' . "\r\n" .
-    'Reply-To:'.$email . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-$to = "danzins15@gmail.com";
-mail($to,$message,$headers,$email_body);
-echo 'You are booked!';
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+$date = $_POST['date'];
+$servicetimes = $_POST['servicetimes'];
+$nailtechnicians = $_POST['nailtechnicians'];
+
+$formcontent=" From: $name \n Email: $email \n Phone: $phone \n Booking Date: $date \n Service Time: $servicetimes \n Nail Technician: $nailtechnicians \n Message: $message";
+
+$recipient = "loanvi229@gmail.com";
+
+$subject = "Appointment";
+
+$mailheader = "From: $email \r\n";
+
+mail($recipient, $subject, $formcontent, $mailheader);
+echo "You are booked!";
 
 ?>
