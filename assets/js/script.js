312003109d59440ca6b05fa4d4d1d1a3521e2119
Dan Zins <danzins15@gmail.com>
2020-06-07 00:47:37 -0500

    Completed mobile hamburger menu, created waiver form section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
 
+
+// Smooth scroll polyfill javascript code
+var scroll = new SmoothScroll('a[href*="#"]');
+
 // Display current date for input date element
-document.getElementById('datePicker').valueAsDate = new Date();
+// document.getElementById('datePicker').valueAsDate = new Date();
+
+
+$('#Selector').datepicker("setDate", new Date(2008,9,03) );
+
+$(document).ready(function(){
+  $(this).scrollTop(0);
+});
+
+
+
+
+
