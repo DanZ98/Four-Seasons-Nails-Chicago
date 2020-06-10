fd05822d51ea77cce612f455c866c321df5dd208
Dan Zins <danzins15@gmail.com>
2020-06-09 23:39:04 -0500

    Updated files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-
-
 // Smooth scroll polyfill javascript code
 var scroll = new SmoothScroll('a[href*="#"]');
 
@@ -7,12 +5,41 @@ var scroll = new SmoothScroll('a[href*="#"]');
 // document.getElementById('datePicker').valueAsDate = new Date();
 
 
-$('#Selector').datepicker("setDate", new Date(2008,9,03) );
+// Date Picker
+var values = '6.22.2020'.split("."); //Default date
+var parsed_date = new Date(values[14], values[14]-14, values[14]);
 
+$(function() {
+
+$( "#datePicker" ).datepicker({ 
+    changeYear: true,
+    minDate: '13D',
+    maxDate: '',
+    defaultDate: parsed_date,
+});
+});
+
+
+// Mobiel menu click function
+$(function($){
+	$( '.menu-btn' ).click(function(){
+	$('.responsive-menu').toggleClass('expand')
+	})
+})
+
+$(document).ready(function () {
+	$(".gallery-img").click(function(){
+	  var t = $(this).attr("src");
+	  $(".modal-body").html("<img src='"+t+"' class='modal-img'>");
+	  $("#myModal").modal();
+	});
+  });
+
+
+// Refresh scroll to top
 $(document).ready(function(){
   $(this).scrollTop(0);
 });
-
 
 
 
--- assets/php/contact.php
+++ assets/php/contact.php
@@ -10,13 +10,14 @@ $nailtechnicians = $_POST['nailtechnicians'];
 
 $formcontent=" From: $name \n Email: $email \n Phone: $phone \n Booking Date: $date \n Service Time: $servicetimes \n Nail Technician: $nailtechnicians \n Message: $message";
 
-$recipient = "loanvi229@gmail.com";
+$recipient = "danzins15@gmail.com";
 
 $subject = "Appointment";
 
 $mailheader = "From: $email \r\n";
 
 mail($recipient, $subject, $formcontent, $mailheader);
-echo "You are booked!";
 
+echo "<p align='center'> <font color=#f0515f  size='6pt'>YOU ARE BOOKED</font> </p>";
+echo "<script>setTimeout(\"location.href = 'http://testingitnownotlive.com/';\",1500);</script>";
 ?>
